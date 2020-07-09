ef2cd7920dbd4e7c6e71612c736624bb7972052b
Ronnie <ronniedipple@gmail.com>
2020-07-08 20:20:17 -0500

    Bottom Nav enabled

---
+++ app/src/main/java/com/example/newsappmvvm/fragments/BreakingNewsFragment.kt
@@ -0,0 +1,8 @@
+package com.example.newsappmvvm.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.newsappmvvm.R
+
+
+class BreakingNewsFragment: Fragment (R.layout.fragment_breaking_news){
+}
